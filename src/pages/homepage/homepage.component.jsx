e2e7cf1a1f11caecc721658b3ee6d059efe4c0fb
Salar Khan <salarkhan586@gmail.com>
2021-04-07 11:47:09 +0530

    Broken our homepage into smaller Components and did some styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './hompage.styles.scss';
+import Directory from '../../components/directory-menu/directory-menu.component'
+const HomePage= ()=>(
+<div className="homepage">
+<Directory></Directory>
+</div>
+
+)
+export default HomePage;
