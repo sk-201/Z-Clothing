7b44a1af5c800f2de126bcc063f2d872dcf6cb34
Salar Khan <salarkhan586@gmail.com>
2021-04-10 09:53:26 +0530

    Updated our app with three new Components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import {Route , Switch} from 'react-router-dom';
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop_page.component';
 
-const HatsPage=()=>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import './App.css';
 function App() {
   return (
     <div >
       <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route path='/' component={HatsPage}></Route>
+      <Route exact path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
   );
