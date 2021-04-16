cf9cf9071e4c4718c253762adde2fb320bf93fa0
Salar Khan <salarkhan586@gmail.com>
2021-04-16 06:55:03 +0530

    Added form-input, button component and did some styling in si

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route , Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop_page.component';
 import Header from './components/header/header.component';
+import SignInandSignup from './pages/signinandsignuppage/sign-in-and-sign-up-page.component';
 
 import './App.css';
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={ShopPage}></Route>
+      <Route exact path='/signin'component={SignInandSignup}></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/signinandsignuppage/sign-in-and-sign-up-page.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+import './sign-in-and-sign-up-page.styles.scss'
+const SignInandSignUp=()=>(
+<div className='sign-in-and-sign-up-page' ><SignIn/></div>
+
+)
+export default SignInandSignUp;
