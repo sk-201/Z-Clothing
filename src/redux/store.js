25e3b01875d0d59b301c5f860f2d49511499c598
Salar Khan <salarkhan586@gmail.com>
2021-05-26 17:27:51 +0530

    Updated our App to redirect to home page if the user is signed in

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import{createStore,applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
-const middlewwares=[logger];
-const store= createStore(rootReducer, applyMiddleware(...middlewwares));
+const middlewares=[logger];
+const store= createStore(rootReducer, applyMiddleware(...middlewares));
 export default store;
